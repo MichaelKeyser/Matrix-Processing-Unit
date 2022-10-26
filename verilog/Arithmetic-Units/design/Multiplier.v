7423c5393ec398c084c116d4314d4ffa3ff32506
Michael Keyser <ma.keyser28@gmail.com>
2022-10-25 18:03:37 -0600

    added multiplier

--- verilog/Arithmetic-Units/design/Multiplier.v
+++ verilog/Arithmetic-Units/design/Multiplier.v
@@ -1,8 +1,14 @@
 module Multiplier
 #(parameter num_bits = 512)
 (input  [num_bits-1:0] dd, aa,
-input clk, reset,
-output [num_bits-1:0] product,
-output reg done_flag);
+output [num_bits-1:0] product);
+
+genvar i;
+generate
+    for (i = 0; i < num_bits; i = i + 8) // Go through the 64 8-bit numbers in the 512-bit chunk
+    begin
+        assign product[i+7:i] = dd[i+7:i] * aa[i+7:i]; // Multiply each 8-bit number, ignore overflow errors
+    end
+endgenerate
 
 endmodule
