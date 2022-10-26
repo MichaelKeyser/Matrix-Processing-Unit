26e235133503f63caeced18d62bf3664a01f996a
Michael Keyser <ma.keyser28@gmail.com>
2022-10-25 18:02:01 -0600

    subtractor done

--- verilog/Arithmetic-Units/design/Subtractor.v
+++ verilog/Arithmetic-Units/design/Subtractor.v
@@ -1,8 +1,13 @@
 module Subtractor
 #(parameter num_bits = 512)
 (input  [num_bits-1:0] dd, aa,
-input clk, reset,
-output [num_bits-1:0] difference,
-output reg done_flag);
+output [num_bits-1:0] difference);
 
+genvar i;
+generate
+    for (i = 0; i < num_bits; i = i + 8) // Go through the 64 8-bit numbers in the 512-bit chunk
+    begin
+        assign difference[i+7:i] = dd[i+7:i] - aa[i+7:i]; // Subtract each 8-bit number
+    end
+endgenerate
 endmodule
