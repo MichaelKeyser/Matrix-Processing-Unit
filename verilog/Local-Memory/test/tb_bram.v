a033060ddf044e65560887613cb7121f361747c5
jbills <jacob.bills@utah.edu>
2022-11-01 16:15:29 -0600

    Started bram testbench

--- verilog/Local-Memory/test/tb_bram.v
+++ verilog/Local-Memory/test/tb_bram.v
@@ -1,49 +1,78 @@
 module tb_bram;
 
-reg[7:0] data;
-reg[11:0] addr;
-reg clk, wre;
-wire[7:0] q;
+parameter num_bits = 512;
 
-bram uut
-(
-    .data(data),
-    .addr(addr),
-    .wre(wre),
-    .clk(clk),
-    .q(q)
-);
+reg [num_bits-1:0] chunk_input;
+reg [7:0] host_input, offset;
+reg line_read_from_host, chunk_read_from_bram, rst, clk;
+wire [7:0] bram_to_host;
+wire [num_bits-1:0] chunk_out;
+
+bram #(.num_bits(num_bits)) uut
+	(
+		.chunk_input(chunk_input),
+		.host_input(host_input),
+		.offset(offset),
+		.line_read_from_host(line_read_from_host),
+		.chunk_read_from_bram(chunk_read_from_bram),
+		.rst(rst),
+		.clk(clk),
+		.bram_to_host(bram_to_host),
+		.chunk_out(chunk_out)
+	);
 
 always #5 clk = ~clk;
 
+integer i;
+
 initial begin
+    clk = 1'b0; rst = 1'b0;
     #10;
-    data = 0; addr = 0; wre = 0; clk = 0;// Initialize all signals
+    rst = 1'b1;
+    #10;
+    rst = 1'b0;
+
+    // Write to alternating bits and check
+
+    for (i = 0; i < num_bits; i = i + 2) 
+    begin
+        chunk_input[i] = 1'b1;
+        chunk_input[i+1] = 1'b0;
+    end
+
+    chunk_read_from_bram = 1'b1;
+    #10;
+    chunk_read_from_bram = 1'b0;
     #10;
 
-    wre = 1;// Set write enable
-    addr = 12'hAAA;// Set address
-    data = 8'hAA;// Set data
+    for (i = 0; i < num_bits; i = i + 2) 
+    begin
+        if (chunk_out[i] != 1'b1 || chunk_out[i+1] != 1'b0)
+            begin
+                $display("chunk_out[%d] = %b%b", i, chunk_out[i], chunk_out[i+1]);
+            end
+    end
+
+    for (i = 0; i < num_bits; i = i + 2) 
+    begin
+        chunk_input[i] = 1'b1;
+        chunk_input[i+1] = 1'b0;
+    end
+
+    chunk_read_from_bram = 1'b1;
     #10;
-    
-    addr = 12'hFFF;// Set address
-    data = 8'hFF;// Set data
+    chunk_read_from_bram = 1'b0;
     #10;
 
-    wre = 0;// Clear write enable
-    addr = 12'hAAA;// Set address
-    #10;
-    $display("q = %h", q);// Display output
+    for (i = 0; i < num_bits; i = i + 2) 
+    begin
+        if (chunk_out[i] != 1'b1 || chunk_out[i+1] != 1'b0)
+            begin
+                $display("chunk_out[%d] = %b%b", i, chunk_out[i], chunk_out[i+1]);
+            end
+    end
 
-    addr = 12'hFFF;// Set address
-    #10;
-    $display("q = %h", q);// Display output
-
-    addr = 12'hAAA;// Set address
-    #10;
-    $display("q = %h", q);// Display output
 
     $finish;
 end
-
 endmodule
