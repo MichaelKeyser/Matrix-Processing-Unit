0bc8f06eb3ec2324807aa2096062c65e6cfd5cfa
Michael Keyser <ma.keyser28@gmail.com>
2022-11-01 16:16:18 -0600

    adder testbench

---
+++ verilog/Arithmetic-Units/test/tb_adder.v
@@ -0,0 +1,18 @@
+module tb_Adder;
+
+parameter num_bits = 512;
+
+reg [num_bits-1:0] dd, aa;
+wire[num_bits - 1:0] sum;
+
+Adder #(num_bits) uut
+(.dd(dd), .aa(aa),
+.sum(sum));
+
+initial begin
+
+
+$finish;
+end
+
+endmodule 
