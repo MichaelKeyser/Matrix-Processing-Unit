5014a602a8eaafea58ca9297d224ade0f54e38ed
jbills <jacob.bills@utah.edu>
2022-11-10 17:34:28 -0700

    Updated ISA and started FSM

--- verilog/FSM/design/FSM.v
+++ verilog/FSM/design/FSM.v
@@ -3,190 +3,198 @@ module FSM
 (
     input [7:0] host_instruction,
     input add1_done, add2_done, sub_done, mult_done, bram0_done, bram1_done, bram2_done, bram3_done, clk, reset,
-    output FILL_ME_IN
+	output reg [8:0] offset,
+	output reg [1:0] aa_MUX, dd_MUX, bram_MUX, out_MUX, host_out_MUX,
+    output reg busy, bram_in_MUX, b0_en, b1_en, b2_en, b3_en, b0_rst, b1_rst, b2_rst, b3_rst, b0_en1, b1_en1, b2_en1, b3_en1
 );
 
-parameter [3:0] S0=4'h0,S1=4'h1,S2=4'h2,S3=4'h3,S4=4'h4,S5=4'h5,S6=4'h6,S7=4'h7,S8=4'h8,S9=4'h9,
-                SA=4'hA,SB=4'hB,SC=4'hC,SD=4'hD,SE=4'hE,SF=4'hF;
+parameter [3:0] RESET = 4'h0, IDLE = 4'h0, ADD = 4'h1, SUB = 4'h2, SHIFT = 4'h3, MULT = 4'h4, LOAD = 4'h5, UNLOAD = 4'h6, COPY = 4'h7, CLEAR = 4'h8;
 					 
-parameter NA =     8'bXXXX00XX;//Do Nothing
-parameter LOAD =   8'bXXXX0100;//Load to BRAM from Host
-parameter UNLOAD = 8'bXXXX0110;//Unload from BRAM to Host
-parameter COPY =   8'bXXXX0101;//Copy from BRAM to BRAM
-parameter CLEAR =  8'bXXXX0111;//Clear BRAM
-parameter ADD1 =   8'bXXXX1100;//Add from BRAM to BRAM using addr 1
-parameter ADD2 =   8'bXXXX1101;//Add from BRAM to BRAM using addr 2
-parameter SUB =    8'bXXXX1110;//Subtract from BRAM to BRAM
-parameter MULT =   8'bXXXX1111;//Multiply from BRAM to BRAM
+parameter NA_OP =     8'bXXXX00XX;//Do Nothing
+parameter LOAD_OP =   8'bXXXX0100;//Load to BRAM from Host
+parameter UNLOAD_OP = 8'bXXXX0110;//Unload from BRAM to Host
+parameter COPY_OP =   8'bXXXX0101;//Copy from BRAM to BRAM
+parameter CLEAR_OP =  8'bXXXX0111;//Clear BRAM
+parameter ADD_OP =   8'bXXXX1100;//Add from BRAM to BRAM using addr 1
+parameter SHIFT_OP =   8'bXXXX1101;//Add from BRAM to BRAM using addr 2
+parameter SUB_OP =    8'bXXXX1110;//Subtract from BRAM to BRAM
+parameter MULT_OP =   8'bXXXX1111;//Multiply from BRAM to BRAM
 
 parameter B1 = 2'b00;//BRAM 1
 parameter B2 = 2'b01;//BRAM 2
 parameter B3 = 2'b10;//BRAM 3
 parameter B4 = 2'b11;//BRAM 4
 
+parameter ADDER = 2'b00;//Adder
+parameter SUBTRACTOR = 2'b10;//Subtractor
+parameter MULTIPLIER = 2'b11;//Multiplier
+parameter SHIFTER = 2'b01;//Shifter
 
-always @(posedge clk)begin
-	if(rst == 1) y <= S0;
-	else begin
-		case(y)
-			S0: y <= S1;			
-			S1: y <= S2;			
-			S2: y <= S3;			
-			S3: y <= S4;			
-			S4: y <= S5;			
-			S5: y <= S6;			
-			S6: y <= S7;			
-			S7: y <= S8;			
-			S8: y <= S9;			
-			S9: y <= SA;			
-			SA: y <= SB;			
-			SB: y <= SC;			
-			SC: y <= SD;			
-			SD: y <= SE;			
-			SE: y <= SF;			
-			SF: y <= SF;			
-			default: y <=4'hx;
-			
-		endcase
-	end
-end
+parameter DD = host_instruction[7-:2];//Shortcut to control lines for DD
+parameter AA = host_instruction[5-:2];//Shortcut to control lines for AA
 
+reg [3:0] state;//Register for the state of the FSM
+reg [5:0] counter;//Register for the counter
 
-always @(y)begin
-	regRst = 0;
-	register_enable = RNA;
-	flgEn = 1'b1;
-	imm_value =8'b00000000;
-	opcode= NOP;
-	a_mux_control = 4'b0000;
-	b_mux_control = 4'b0000;
-	enable_seven_seg = 1'b0;
-	
-	
-	case(y)
-			S0:begin//Reset everything
-				regRst = 1;//Reset all registers to 0
+assign aa_MUX = AA;//Control the AA MUX with the AA bits from the instruction
+assign dd_MUX = DD;//Control the DD MUX with the DD bits from the instruction
+assign bram_MUX = AA;//Control the BRAM MUX with the AA bits from the instruction
+
+always @(posedge clk, posedge rst)begin
+	if(rst == 1) state <= RESET;
+
+	busy = 1;//It's busy in all states but IDLE
+
+	//Default BRAM controls
+	b0_en1 = 0; b0_en = 0; b0_rst = 0;
+	b1_en1 = 0; b1_en = 0; b1_rst = 0;
+	b2_en1 = 0; b2_en = 0; b2_rst = 0;
+	b3_en1 = 0; b3_en = 0; b3_rst = 0;
+
+	bram_in_MUX = 0;//Only COPY sets to 1
+
+	case(state)
+		RESET: begin//Reset all BRAMS and state
+			b0_rst = 1;
+			b1_rst = 1;
+			b2_rst = 1;
+			b3_rst = 1;
+
+			state <= IDLE;//Return to IDLE	
+		end
+
+		IDLE: begin//Check for instruction
+			busy = 0;//Indicate that it's not busy
+			counter = 0;//Reset counter
+			offset = 7;//Reset offset
+
+			//Check for instruction and set state
+			if(host_instruction == LOAD_OP) begin
+				state = LOAD;
 			end
-	
-			S1:begin//1 will be seen on the data path
-				register_enable = 16'b1100000000000000;
-				imm_value =8'b00000001;
-
-				opcode= ADDUI;		//ADDI (1+0 == 1)
-				a_mux_control = 4'h0;//R0
-				b_mux_control = 4'hx;//Any since using imm
+			else if(host_instruction == UNLOAD_OP) begin
+				state = UNLOAD;
 			end
-			
-			S2:begin//1+1 =   2 will be seen on the data path, 2 is put in R02
-				register_enable = R02;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h0;
-				b_mux_control = 4'h1;
+			else if(host_instruction == COPY_OP) begin
+				state = COPY;
 			end
-			
-			S3:begin //1+2 =   3 will be seen on the data path, 3 is put in R03
-				register_enable = R03;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h1;
-				b_mux_control = 4'h2;
+			else if(host_instruction == CLEAR_OP) begin
+				state = CLEAR;
 			end
-			
-			S4:begin//3+2 =   5 will be seen on the data path, 5 is put in R04
-				register_enable = R04;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h2;
-				b_mux_control = 4'h3;
+			else if(host_instruction == ADD_OP) begin
+				state = ADD;
 			end
-			
-			S5:begin//5+3 =   8 will be seen on the data path, 8 is put in R05
-				register_enable = R05;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h3;
-				b_mux_control = 4'h4;
+			else if(host_instruction == SHIFT_OP) begin
+				state = SHIFT;
 			end
-			
-			S6:begin //etc
-				register_enable = R06;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h4;
-				b_mux_control = 4'h5;
+			else if(host_instruction == SUB_OP) begin
+				state = SUB;
 			end
-			
-			S7:begin
-				register_enable = R07;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h5;
-				b_mux_control = 4'h6;
+			else if(host_instruction == MULT_OP) begin
+				state = MULT;
 			end
-			
-			S8:begin
-				register_enable = R08;
-
-				opcode= ADDU;		 
-				a_mux_control = 4'h6;
-				b_mux_control = 4'h7;
+			else begin
+				state = IDLE;//Return to IDLE
 			end
-			
-			S9:begin
-				register_enable = R09;
+		end
 
-				opcode= ADDU;		 
-				a_mux_control = 4'h7;
-				b_mux_control = 4'h8;
-			end
-			
-			SA:begin
-				register_enable = R10;
+		ADD: begin
+			//Enable correct BRAM
+			if(DD = B0) b0_en = 1;
+			else if(DD = B1) b1_en = 1;
+			else if(DD = B2) b2_en = 1;
+			else b3_en = 1;
 
-				opcode= ADDU;		 
-				a_mux_control = 4'h8;
-				b_mux_control = 4'h9;
-			end
-			
-			SB:begin
-				register_enable = R11;
+			out_MUX = ADDER;//Select correct output
 
-				opcode= ADDU;		 
-				a_mux_control = 4'h9;
-				b_mux_control = 4'hA;
-			end
-			
-			SC:begin
-				register_enable = R12;
+			state = IDLE;//Return to IDLE
+		end
 
-				opcode= ADDU;		 
-				a_mux_control = 4'hA;
-				b_mux_control = 4'hB;
-			end
-			
-			SD:begin
-				register_enable = R13;
+		SUB: begin
+			//Enable correct BRAM
+			if(DD = B0) b0_en = 1;
+			else if(DD = B1) b1_en = 1;
+			else if(DD = B2) b2_en = 1;
+			else b3_en = 1;
 
-				opcode= ADDU;		 
-				a_mux_control = 4'hB;
-				b_mux_control = 4'hC;
-			end
-			
-			SE:begin
-				register_enable = R14;
+			out_MUX = SUBTRACTOR;//Select correct output
 
-				opcode= ADDU;		 
-				a_mux_control = 4'hC;
-				b_mux_control = 4'hD;
-			end
-			
-			SF:begin
-				register_enable = R15;
+			state = IDLE;//Return to IDLE
+		end
 
-				opcode= ADDU;		 
-				a_mux_control = 4'hD;
-				b_mux_control = 4'hE;
+		SHIFT: begin
+			//Enable correct BRAM
+			if(DD = B0) b0_en = 1;
+			else if(DD = B1) b1_en = 1;
+			else if(DD = B2) b2_en = 1;
+			else b3_en = 1;
+
+			out_MUX = SHIFTER;//Select correct output
+
+			state = IDLE;//Return to IDLE
+		end
+
+		MULT: begin
+			//Enable correct BRAM
+			if(DD = B0) b0_en = 1;
+			else if(DD = B1) b1_en = 1;
+			else if(DD = B2) b2_en = 1;
+			else b3_en = 1;
+
+			out_MUX = MULTIPLIER;//Select correct output
+
+			state = IDLE;//Return to IDLE
+		end
+
+		LOAD: begin
+			//Enable correct BRAM single load
+			if(DD = B0) b0_en1 = 1;
+			else if(DD = B1) b1_en1 = 1;
+			else if(DD = B2) b2_en 1= 1;
+			else b3_en1 = 1;
+
+			if(counter >= 63) begin//Check if done 64 loads
+				state = IDLE;//Return to IDLE
 			end
+
+			else state = LOAD;//Stay in LOAD
+
+			counter = counter + 1;//Increment counter
+			offset = offset + 7;//Increment offset by 7
+		end
+
+		UNLOAD: begin
+			if(counter >= 63) state = IDLE;//If done 64 unloads, return to IDLE
+			else state = UNLOAD;//Stay in UNLOAD
+
+			counter = counter + 1;//Increment counter
+			offset = offset + 7;//Increment offset by 7
+
+		end
+
+		COPY: begin
+			//Enable correct BRAM
+			if(DD = B0) b0_en = 1;
+			else if(DD = B1) b1_en = 1;
+			else if(DD = B2) b2_en = 1;
+			else b3_en = 1;
+
+			bram_in_MUX = 1;//Select BRAM input
+
+			state = IDLE;//Return to IDLE
+		end
+
+		CLEAR: begin
+			if(DD = B0) b0_rst = 1;
+			else if(DD = B1) b1_rst = 1;
+			else if(DD = B2) b2_rst = 1;
+			else b3_rst = 1;
+
+			state = IDLE;//Return to IDLE
+		end
+		
+		default: state = RESET;//Reset if something goes wrong
 	endcase
 end
+
+endmodule
