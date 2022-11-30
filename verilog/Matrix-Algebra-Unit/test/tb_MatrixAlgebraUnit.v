4b486355bed0587a0b49acf970576817911472c6
JACOB BILLS <u1012574@umail.utah.edu>
2022-11-30 11:26:42 -0700

    Started fixing design timing for modelsim

--- verilog/Matrix-Algebra-Unit/test/tb_MatrixAlgebraUnit.v
+++ verilog/Matrix-Algebra-Unit/test/tb_MatrixAlgebraUnit.v
@@ -33,7 +33,7 @@ always begin
     clk = ~clk;
 end
 
-//always @ (posedge clk) if(uut.b0_line_read_from_host && clk) $display("data_in = %h | Offset = %d", data_in, uut.B0.offset);
+//always @ (posedge clk) if(uut.b3_line_read_from_host && clk) $display("data_in = %h | Offset = %d", uut.B3.host_input, uut.B3.offset);
 
 integer i;
 integer do;
@@ -59,34 +59,33 @@ if(busy_flag == BUSY) $display("NOP failed\n");
 data_in = 8'h01;
 #40;
 test_load_val = 8'b0;
-data_in = 8'h01;
+data_in = 8'h00;
 host_instruction = 8'b00_00_01_00;//LOAD from host to BRAM 0
 #20;
 while(busy_flag == BUSY) begin
-    data_in = data_in + 8'h01;
     host_instruction = 8'b00_00_00_00;//NOP
+    data_in = data_in + 8'h01;
     #10;
 end
+
 host_instruction = 8'b00_00_00_00;//NOP
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
     if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
 end
-#20;
-
-
+#50;
 
 // BRAM 1
 data_in = 8'h01;
 #40;
 test_load_val = 8'h00;
-data_in = 8'h01;
-host_instruction = 8'b01_00_01_00;//LOAD from host to BRAM 0
-#30;
+data_in = 8'h00;
+host_instruction = 8'b01_00_01_00;//LOAD from host to BRAM 1
+#20;
 while(busy_flag == BUSY) begin
-    data_in = data_in + 8'h01;
     host_instruction = 8'b01_00_00_00;//NOP
+    data_in = data_in + 8'h01;
     #10;
 end
 host_instruction = 8'b00_00_00_00;//NOP
@@ -95,13 +94,14 @@ for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
     if(uut.b1_chunk_out[i-:8] != test_load_val) $display("LOAD B1 failed at %d | Got = %h | Expected = %h", i, uut.b1_chunk_out[i], test_load_val);
 end
+#20;
 
 // BRAM 2
 data_in = 8'h01;
 #40;
 test_load_val = 8'b0;
-data_in = 8'h01;
-host_instruction = 8'b10_00_01_00;//LOAD from host to BRAM 0
+data_in = 8'h00;
+host_instruction = 8'b10_00_01_00;//LOAD from host to BRAM 2
 #20;
 while(busy_flag == BUSY) begin
     data_in = data_in + 8'h01;
@@ -114,12 +114,13 @@ for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
     if(uut.b2_chunk_out[i-:8] != test_load_val) $display("LOAD B2 failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val);
 end
+#20;
 
 // BRAM 3
 data_in = 8'h01;
 #40;
 test_load_val = 8'b0;
-data_in = 8'h01;
+data_in = 8'h00;
 host_instruction = 8'b11_00_01_00;//LOAD from host to BRAM 0
 #20;
 while(busy_flag == BUSY) begin
@@ -133,6 +134,7 @@ for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
     if(uut.b3_chunk_out[i-:8] != test_load_val) $display("LOAD B3 failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val);
 end
+#20;
 
 /* TESTING UNLOAD COMMAND for each BRAM */
 // BRAM 0
@@ -194,9 +196,10 @@ host_instruction = 8'b00_00_00_00;//NOP
 /* TESTING CLEAR COMMAND for all BRAMS */
 //CLEAR BRAM 0
 host_instruction = 8'b00_00_01_11;
-#20;
+#30;
 if(uut.b0_chunk_out != 0) begin
     $display("CLEAR BRAM 0: Failed");
+    $display("%b", uut.b0_chunk_out);
     end
 while(busy_flag) begin
     host_instruction = 8'b00000000;
@@ -205,7 +208,7 @@ end
 
 //CLEAR BRAM 1
 host_instruction = 8'b01_00_01_11;
-#20;
+#30;
 if(uut.b1_chunk_out != 0) begin
     $display("CLEAR BRAM 1: Failed");
     end
@@ -216,7 +219,7 @@ end
 
 //CLEAR BRAM 2
 host_instruction = 8'b10_00_01_11;
-#20;
+#30;
 if(uut.b2_chunk_out != 0) begin
     $display("CLEAR BRAM 2: Failed");
 end
@@ -227,7 +230,7 @@ end
 
 //CLEAR BRAM 3
 host_instruction = 8'b11_00_01_11;
-#20;
+#30;
 if(uut.b3_chunk_out != 0) begin
     $display("CLEAR BRAM 3: Failed");
 end
@@ -241,25 +244,26 @@ end
 data_in = 8'h01;
 #40;
 test_load_val = 8'b0;
-data_in = 8'h01;
+data_in = 8'h00;
 host_instruction = 8'b00_00_01_00;//LOAD from host to BRAM 0
 #20;
 while(busy_flag == BUSY) begin
-    data_in = data_in + 8'h01;
     host_instruction = 8'b00_00_00_00;//NOP
+    data_in = data_in + 8'h01;
     #10;
 end
+
 host_instruction = 8'b00_00_00_00;//NOP
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
-    if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
+    if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 for COPY failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
 end
-#20;
+#50;
 
 //COPY BRAM 0 to BRAM 1
 host_instruction = 8'b01_00_01_01;//COPY BRAM 0 to BRAM 1
-#20;
+#30;
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
@@ -272,7 +276,7 @@ end
 
 //COPY BRAM 1 to BRAM 2
 host_instruction = 8'b10_01_01_01;//COPY BRAM 1 to BRAM 2
-#20;
+#30;
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
@@ -285,7 +289,7 @@ end
 
 //COPY BRAM 2 to BRAM 3
 host_instruction = 8'b11_10_01_01;//COPY BRAM 1 to BRAM 2
-#20;
+#30;
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
@@ -296,11 +300,9 @@ while(busy) begin
     #10;
 end
 
-//$display("%h\n%h\n%h\n%h", uut.b0_chunk_out, uut.b1_chunk_out, uut.b2_chunk_out, uut.b3_chunk_out);
-
 /* TESTING ADD COMMAND for all BRAMS */
 host_instruction = 8'b00_01_11_00;//BRAM 0 = BRAM 0 + BRAM 1
-#20;
+#40;
 test_load_val = 8'h00;
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
@@ -310,489 +312,21 @@ while(busy) begin
     host_instruction = 8'b00000000;
     #10;
 end
+#50;
 
 host_instruction = 8'b10_11_11_00;//BRAM 2 = BRAM 2 + BRAM 3
-#20;
+#40;
 test_load_val = 8'h00;
+while(busy) begin
+    host_instruction = 8'b00000000;
+    #10;
+end
 for(i = 7; i < 512; i = i + 8) begin
     test_load_val = test_load_val + 8'h01;
-    if(uut.b2_chunk_out[i-:8] != test_load_val*2) $display("B2 = B0 + B1  failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val*2);
+    if(uut.b2_chunk_out[i-:8] != test_load_val*2) $display("B2 = B2 + B3  failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val*2);
 end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 0
-host_instruction = 8'b00_00_01_11;
-#20;
-if(uut.b0_chunk_out != 0) begin
-    $display("CLEAR BRAM 0: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 1
-host_instruction = 8'b01_00_01_11;
-#20;
-if(uut.b1_chunk_out != 0) begin
-    $display("CLEAR BRAM 1: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 2
-host_instruction = 8'b10_00_01_11;
-#20;
-if(uut.b2_chunk_out != 0) begin
-    $display("CLEAR BRAM 2: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 3
-host_instruction = 8'b11_00_01_11;
-#20;
-if(uut.b3_chunk_out != 0) begin
-    $display("CLEAR BRAM 3: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-/* TESTING SHIFT COMMAND */
-//LOAD BRAM 0 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b00_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b00_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 1 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b01_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b01_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b1_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b1_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 2 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b10_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b10_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 3 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b11_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b11_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b3_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b3_chunk_out[i], test_load_val);
-end
-#10;
-
-//SHIFT BRAM 0 LEFT by BRAM 1
-host_instruction = 8'b00_01_11_01;//BRAM 0 = BRAM 0 << BRAM 1
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != 8'h02) $display("BRAM 0 = BRAM 0 << BRAM 1: Failed");
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//SHIFT BRAM 1 LEFT by BRAM 2
-host_instruction = 8'b01_10_11_01;//BRAM 1 = BRAM 1 << BRAM 2
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b1_chunk_out[i-:8] != 8'h02) $display("BRAM 1 = BRAM 1 << BRAM 2: Failed");
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//SHIFT BRAM 2 LEFT by BRAM 3
-host_instruction = 8'b10_11_11_01;//BRAM 2 = BRAM 2 << BRAM 3
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != 8'h02) $display("BRAM 2 = BRAM 2 << BRAM 3: Failed");
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//SHIFT BRAM 3 LEFT by BRAM 0
-//$display("%d << %d = %d", uut.b3_chunk_out[7-:8], uut.b0_chunk_out[7-:8], uut.b3_chunk_out[7-:8] << uut.b0_chunk_out[7-:8]);
-host_instruction = 8'b11_00_11_01;//BRAM 3 = BRAM 3 << BRAM 0
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b3_chunk_out[i-:8] != 8'h04) $display("BRAM 3 = BRAM 3 << BRAM 0: Failed | Result = %d | %h | %b", uut.b3_chunk_out[i-:8], uut.shifter_out[i-:8], uut.arithmetic_mux_out_sel);
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 0
-host_instruction = 8'b00_00_01_11;
-#20;
-if(uut.b0_chunk_out != 0) begin
-    $display("CLEAR BRAM 0: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 1
-host_instruction = 8'b01_00_01_11;
-#20;
-if(uut.b1_chunk_out != 0) begin
-    $display("CLEAR BRAM 1: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 2
-host_instruction = 8'b10_00_01_11;
-#20;
-if(uut.b2_chunk_out != 0) begin
-    $display("CLEAR BRAM 2: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 3
-host_instruction = 8'b11_00_01_11;
-#20;
-if(uut.b3_chunk_out != 0) begin
-    $display("CLEAR BRAM 3: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-/* TESTING SUB COMMAND for all BRAMS */
-//LOAD BRAM 0 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b00_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b00_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 1 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b01_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b01_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b1_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b1_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 2 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b10_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b10_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 3 with 1's
-data_in = 8'h01;
-#40;
-test_load_val = 8'h01;
-data_in = 8'h01;
-host_instruction = 8'b11_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b11_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h01;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b3_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b3_chunk_out[i], test_load_val);
-end
-#30;
-
-host_instruction = 8'b00_01_11_10;//BRAM 0 = BRAM 0 - BRAM 1
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != 8'b0) $display("B0 = B0 - B1  failed at %d | Got = %h | %b", i, uut.b0_chunk_out[i-:8], uut.arithmetic_mux_out_sel);
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-host_instruction = 8'b10_11_11_10;//BRAM 2 = BRAM 2 - BRAM 3
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != 8'b0) $display("B2 = B0 - B1  failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i-:8], 0);
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 1
-host_instruction = 8'b01_00_01_11;
-#20;
-if(uut.b1_chunk_out != 0) begin
-    $display("CLEAR BRAM 1: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 2
-host_instruction = 8'b10_00_01_11;
-#20;
-if(uut.b2_chunk_out != 0) begin
-    $display("CLEAR BRAM 2: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 3
-host_instruction = 8'b11_00_01_11;
-#20;
-if(uut.b3_chunk_out != 0) begin
-    $display("CLEAR BRAM 3: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-/* TESTING MULT COMMAND for all BRAMS */
-//LOAD BRAM 0 with 2's
-data_in = 8'h02;
-#40;
-host_instruction = 8'b00_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b00_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h02;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b0_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 1 with 3's
-data_in = 8'h03;
-#40;
-host_instruction = 8'b01_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b01_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h03;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b1_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b1_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 2 with 4's
-data_in = 8'h04;
-#40;
-host_instruction = 8'b10_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b10_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h04;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b2_chunk_out[i], test_load_val);
-end
-#10;
-
-//LOAD BRAM 3 with 5's
-data_in = 8'h05;
-#40;
-host_instruction = 8'b11_00_01_00;//LOAD from host to BRAM 0
-#20;
-while(busy_flag == BUSY) begin
-    host_instruction = 8'b11_00_00_00;//NOP
-    #10;
-end
-host_instruction = 8'b00_00_00_00;//NOP
-test_load_val = 8'h05;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b3_chunk_out[i-:8] != test_load_val) $display("LOAD B0 failed at %d | Got = %h | Expected = %h", i, uut.b3_chunk_out[i], test_load_val);
-end
-#30;
-
-host_instruction = 8'b00_01_11_11;//BRAM 0 = BRAM 0 * BRAM 1
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b0_chunk_out[i-:8] != 8'd6) $display("B0 = B0 * B1  failed at %d | Got = %h | %b", i, uut.b0_chunk_out[i-:8], uut.arithmetic_mux_out_sel);
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-host_instruction = 8'b10_11_11_11;//BRAM 2 = BRAM 2 * BRAM 3
-#20;
-for(i = 7; i < 512; i = i + 8) begin
-    if(uut.b2_chunk_out[i-:8] != 8'd20) $display("B2 = B0 * B1  failed at %d | Got = %d | Expected = %d", i, uut.b2_chunk_out[i-:8], 8'd20);
-end
-while(busy) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-
-//CLEAR BRAM 1
-host_instruction = 8'b01_00_01_11;
-#20;
-if(uut.b1_chunk_out != 0) begin
-    $display("CLEAR BRAM 1: Failed");
-    end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 2
-host_instruction = 8'b10_00_01_11;
-#20;
-if(uut.b2_chunk_out != 0) begin
-    $display("CLEAR BRAM 2: Failed");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
-//CLEAR BRAM 3
-host_instruction = 8'b11_00_01_11;
-#20;
-if(uut.b3_chunk_out != 0) begin
-    $display("%b", uut.b3_chunk_out);
-    $display("CLEAR BRAM 3: Failed!");
-end
-while(busy_flag) begin
-    host_instruction = 8'b00000000;
-    #10;
-end
-#10;
-
 
 $finish;
-
-
-
 end
 
 endmodule
